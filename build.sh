cd084ff7edcbdf69439663e8b8f44b2a4069a725
Stuart Douglas <stuart.w.douglas@gmail.com>
2022-03-10 16:02:26 +1100

    Initial

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+mkdir -p dist/boot
+mkdir -p dist/lib
+mkdir -p dist/bin
+mkdir -p dist/conf
+
+
+cd projects/plexus-classworlds
+./build.sh
---
+++ projects/plexus-classworlds/build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+git clone https://github.com/codehaus-plexus/plexus-classworlds.git code-checkout
+cd code-checkout
+git checkout plexus-classworlds-2.6.0
+mkdir -p target
+javac -d target/classes `find src/main/java/ -name \*.java`
+jar -c --file target/plexus-classworlds-2.6.0.jar target/classes
+
+cp target/plexus-classworlds-2.6.0.jar ../../../dist/boot/
+cp LICENSE-2.0.txt ../../../dist/boot/plexus-classworlds.license
